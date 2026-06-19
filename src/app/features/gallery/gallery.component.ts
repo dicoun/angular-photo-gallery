9919b586601bd647271d9ebf537989fe3c1761c2
Volha Dikun <dikun.volha@gmail.com>
2026-06-19 15:10:02 +0300

    feat: add gallery component

--- src/app/features/gallery/gallery.component.ts
+++ src/app/features/gallery/gallery.component.ts
@@ -1,11 +1,49 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Photo } from 'src/app/core/models/photo.model';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatGridListModule],
 })
-export class GalleryComponent {}
+export class GalleryComponent {
+  protected photos = signal<Array<Photo>>([
+    {
+      id: 'url_1',
+      url: 'Test_url_1',
+    },
+    {
+      id: 'url_2',
+      url: 'Test_url_2',
+    },
+    {
+      id: 'url_3',
+      url: 'Test_url_3',
+    },
+  ]);
+
+  private isLoading = signal<boolean>(false);
+
+  public ngOnInit(): void {
+    this.getPhotos();
+  }
+
+  protected trackById(index: number, photo: Photo): string {
+    return photo.id;
+  }
+
+  protected addToFavoritPhotos(photo: Photo): void {
+    //add photo to favorites
+  }
+
+  private getPhotos(): void {
+    if (this.isLoading()) return;
+
+    this.isLoading.set(true);
+    //get photos from service
+  }
+}
