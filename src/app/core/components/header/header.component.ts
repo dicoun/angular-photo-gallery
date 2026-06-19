f752c1275791598c8671d03f97681831ab0afac2
Volha Dikun <dikun.volha@gmail.com>
2026-06-19 16:54:32 +0300

    feat: add header component

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, MatButtonModule],
+})
+export class HeaderComponent {}
