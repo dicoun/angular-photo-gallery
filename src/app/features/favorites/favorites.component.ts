1d53aefc24d6a5960da3f133b39b774040f0384b
Volha Dikun <dikun.volha@gmail.com>
2026-06-19 18:27:21 +0300

    feat: add favorites component

--- src/app/features/favorites/favorites.component.ts
+++ src/app/features/favorites/favorites.component.ts
@@ -1,11 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { RouterModule } from '@angular/router';
+import { Photo } from 'src/app/core/models/photo.model';
+import { FavoritesService } from 'src/app/services/favorites-service/favorites.service';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatGridListModule, RouterModule],
 })
-export class FavoritesComponent {}
+export class FavoritesComponent {
+  private readonly favoritesService = inject(FavoritesService);
+  protected readonly favoritePhotos = this.favoritesService.favorites;
+
+  protected trackById(_: number, photo: Photo): string {
+    return photo.id;
+  }
+}
