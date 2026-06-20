502a9780a7cec28fcca47907f6199b714fe62b37
Volha Dikun <dikun.volha@gmail.com>
2026-06-20 18:53:52 +0300

    feat: add unit tests for header and favorites components

--- src/app/core/components/header/header.component.spec.ts
+++ src/app/core/components/header/header.component.spec.ts
@@ -1,21 +1,56 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Directive } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { By } from '@angular/platform-browser';
+import { FavoritesComponent } from 'src/app/features/favorites/favorites.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      imports: [
+        HeaderComponent,
+        RouterTestingModule.withRoutes([
+          { path: 'favorites', component: FavoritesComponent },
+        ]),
+      ],
     });
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a relevant navigation links', () => {
+    const allElements = fixture.debugElement.queryAll(By.directive(RouterLink));
+
+    expect(allElements[0].attributes['routerLink']).toBe('/');
+    expect(allElements[1].attributes['routerLink']).toBe('/favorites');
+  });
+
+  it('should show correct text on navigation buttons', () => {
+    const buttons = fixture.nativeElement.querySelectorAll('button');
+
+    expect(buttons[0].textContent.trim()).toBe('Photos');
+    expect(buttons[1].textContent.trim()).toBe('Favorites');
+  });
+
+  it('should add active class to Favorites button when click it', async () => {
+    await router.navigate(['/favorites']);
+    fixture.detectChanges();
+    const buttons = fixture.nativeElement.querySelectorAll('button');
+
+    expect(buttons[0].classList.contains('active')).toBeFalse();
+    expect(buttons[1].classList.contains('active')).toBeTrue();
   });
 });
--- src/app/features/favorites/favorites.component.spec.ts
+++ src/app/features/favorites/favorites.component.spec.ts
@@ -1,14 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FavoritesComponent } from './favorites.component';
+import { FavoritesService } from 'src/app/services/favorites-service/favorites.service';
+import { signal } from '@angular/core';
+import { Photo } from 'src/app/core/models/photo.model';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
 
 describe('FavoritesComponent', () => {
   let component: FavoritesComponent;
   let fixture: ComponentFixture<FavoritesComponent>;
+  let mokeFavorites = signal<Photo[]>([]);
+  let data = [
+    {
+      id: 'b4e95f75',
+      url: 'https://picsum.photos/id/167/200/300',
+    },
+    {
+      id: '5bef952d',
+      url: 'https://picsum.photos/id/312/200/300',
+    },
+  ];
 
   beforeEach(() => {
+    mokeFavorites.set([]);
+
     TestBed.configureTestingModule({
-      imports: [FavoritesComponent],
+      imports: [FavoritesComponent, RouterTestingModule],
+      providers: [
+        {
+          provide: FavoritesService,
+          useValue: {
+            favorites: mokeFavorites.asReadonly(),
+          },
+        },
+      ],
     });
     fixture = TestBed.createComponent(FavoritesComponent);
     component = fixture.componentInstance;
@@ -17,5 +44,43 @@ describe('FavoritesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show message when favorites list is empty', () => {
+    const message = fixture.nativeElement.querySelector('p');
+
+    expect(message).toBeTruthy;
+    expect(message.textContent.trim()).toBe('No favorites photos');
+
+    const list = fixture.nativeElement.querySelector('mat-grid-list');
+
+    expect(list).toBe(null);
+  });
+
+  it('should render favorites list with photos', () => {
+    mokeFavorites.set(data);
+    fixture.detectChanges();
+    const images = fixture.nativeElement.querySelectorAll('img');
+
+    expect(images.length).toBe(2);
+    expect(images[0].src).toContain('https://picsum.photos/id/167/200/300');
+    expect(fixture.nativeElement.textContent).not.toContain(
+      'No favorites photos',
+    );
+  });
+
+  it('should be a relevant router link for a favorite photo', () => {
+    mokeFavorites.set(data);
+    fixture.detectChanges();
+    const element = fixture.debugElement.query(By.directive(RouterLink));
+
+    expect(element).toBeTruthy();
+
+    const routerLink = element.nativeElement.getAttribute(
+      'ng-reflect-router-link',
+    );
+
+    expect(routerLink).toContain('/photos');
+    expect(routerLink).toContain('b4e95f75');
   });
 });
