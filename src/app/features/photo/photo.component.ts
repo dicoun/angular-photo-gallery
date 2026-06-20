2f1cfbd72568d223bfef368aac329c8b5053a049
Volha Dikun <dikun.volha@gmail.com>
2026-06-20 11:36:05 +0300

    feat: add photo component; feat: add styling to the project

--- src/app/features/photo/photo.component.ts
+++ src/app/features/photo/photo.component.ts
@@ -1,11 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, inject, Input, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { Photo } from 'src/app/core/models/photo.model';
+import { FavoritesService } from 'src/app/services/favorites-service/favorites.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-photo',
   templateUrl: './photo.component.html',
   styleUrls: ['./photo.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatProgressSpinnerModule],
 })
-export class PhotoComponent {}
+export class PhotoComponent {
+  private readonly router = inject(Router);
+  private readonly favoritesService = inject(FavoritesService);
+  private readonly signalId = signal<string | undefined>(undefined);
+
+  @Input({ required: true }) set id(id: string) {
+    this.signalId.set(id);
+  }
+
+  protected readonly currentPhoto = computed<Photo | undefined>(() => {
+    const photoId = this.signalId();
+    if (!photoId) return;
+
+    return this.favoritesService.favorites().find((p) => p.id === photoId);
+  });
+
+  protected removePhoto(): void {
+    const photo = this.currentPhoto();
+    if (!photo) return;
+
+    this.favoritesService.deleteFromFavorites(photo);
+    this.router.navigate(['/favorites']);
+  }
+}
